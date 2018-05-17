08112717c15e58e1fba6ed9ab70ccabebfef5322
andy mcghie <mcghiea@gmail.com>
2018-05-17 04:26:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ function updatePlaylist( obj, key, value){
   return Object.assign(obj, {[key]: value})
 };
 function removeFromPlaylist(obj, key){
-  return delete obj.key;
+  return delete obj.key
 
 };
