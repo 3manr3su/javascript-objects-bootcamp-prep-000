de04f405786d0dc202e216ae99548ac8a7694e4c
andy mcghie <mcghiea@gmail.com>
2018-05-17 04:07:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {Coltrane: "Giant Steps"};
 function updatePlaylist( obj, key, value){
-  return Object.assign(obj, {[key]: value})
+  return obj.assign(obj, {[key]: value})
 };
 function removeFromPlaylist(obj, key){
   return obj.delete[key];
